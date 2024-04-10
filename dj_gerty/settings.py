d3bd787cb5e305b1982feaaf3fe191eb31862645
wangyao <wangyao.dream@hotmail.com>
2024-04-10 17:53:37 +0800

    add new app polls

--- dj_gerty/settings.py
+++ dj_gerty/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "members",
     "tasks",
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
--- dj_gerty/urls.py
+++ dj_gerty/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('members.urls')),
-    path('', include('tasks.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('polls/', include('polls.urls'))
 ]
