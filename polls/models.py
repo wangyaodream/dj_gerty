9760deeacd7a7ef634b46ed142e624ddab8a37fd
wangyao <wangyao.dream@hotmail.com>
2024-04-11 16:37:12 +0800

    add new templates index.html and detail.html

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,6 +11,9 @@ class Question(models.Model):
     def __str__(self):
         # 在获取数据库内容比如q.objects.all()，所展示出来的内容
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
