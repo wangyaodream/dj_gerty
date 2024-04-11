52d4a57dfb7d7cc2bff78339e5dfb72ea468af28
wangyao <wangyao.dream@hotmail.com>
2024-04-11 09:14:32 +0800

    add __str__ for polls.models

--- polls/models.py
+++ polls/models.py
@@ -6,10 +6,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
-        #
+        # 在获取数据库内容比如q.objects.all()，所展示出来的内容
+        return self.question_text
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
