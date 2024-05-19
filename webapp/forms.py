5656e33d899ed938bb2eb90fe7bd22fb7a322998
wangyao <wangyao.dream@hotmail.com>
2024-05-19 21:20:47 +0800

    add create user

---
+++ webapp/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms
+
+
+# register/create a user
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
